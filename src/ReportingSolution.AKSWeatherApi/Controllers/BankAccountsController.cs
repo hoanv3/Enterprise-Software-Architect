7a45b2b7ff3d4d6ecf4640c2e6106fb9ee760201
LQA\steven <Steven@ltsgroup.tech>
2025-04-22 15:50:29 +0700

    Small fixes applied.

---
+++ src/ReportingSolution.AKSWeatherApi/Controllers/BankAccountsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportingSolution.Data.EventSourcingModels;
+
+namespace ReportingSolution.AKSWeatherApi.Controllers
+{
+    [ApiController]
+    [Route("api/bank-accounts")]
+    public class BankAccountsController : ControllerBase
+    {
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public BankAccountsController(IBankAccountRepository bankAccountRepository)
+        {
+            _bankAccountRepository = bankAccountRepository;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async ValueTask<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+        {
+            var original = await _bankAccountRepository.GetByIdAsync(id, cancellationToken);
+            if (original is null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(original);
+        }
+
+        [HttpPost("")]
+        public async ValueTask<IActionResult> OpenBankAccount(string owner, CancellationToken cancellationToken)
+        {
+            var account = BankAccount.OpenBankAccount(Guid.NewGuid(), owner);
+            await _bankAccountRepository.SaveAsync(account, cancellationToken);
+            return Ok(account);
+        }
+
+        [HttpPost("{id:guid}/deposit")]
+        public async ValueTask<IActionResult> Deposit(Guid id, decimal amount, CancellationToken cancellationToken)
+        {
+            var original = await _bankAccountRepository.GetByIdAsync(id, cancellationToken);
+            original.Deposit(amount);
+            await _bankAccountRepository.SaveAsync(original, cancellationToken);
+
+            return Ok(original);
+        }
+
+        [HttpPost("{id:guid}/withdraw")]
+        public async ValueTask<IActionResult> Withdraw(Guid id, decimal amount, CancellationToken cancellationToken)
+        {
+            var original = await _bankAccountRepository.GetByIdAsync(id, cancellationToken);
+            original.Withdraw(amount);
+            await _bankAccountRepository.SaveAsync(original, cancellationToken);
+
+            return Ok(original);
+        }
+    }
+}
--- src/ReportingSolution.Infrastructure/Repositories/BankAccountRepository.cs
+++ src/ReportingSolution.Infrastructure/Repositories/BankAccountRepository.cs
@@ -24,7 +24,7 @@ namespace ReportingSolution.Infrastructure.Repositories
 
             // Need to apply source generator
             var domainEvents = events
-                .Select(x => JsonConvert.DeserializeObject<IDomainEvent>(x.Data))
+                .Select(x => JsonConvert.DeserializeObject(x.Data, Type.GetType(x.EventType)!) as IDomainEvent)
                 .Where(x => x is not null)
                 .ToList();
 
@@ -36,15 +36,17 @@ namespace ReportingSolution.Infrastructure.Repositories
         public async ValueTask SaveAsync(BankAccount bankAccount, CancellationToken cancellationToken)
         {
             var domainEvents = bankAccount.GetUncommittedStateChanges();
+            var entities = domainEvents.Select(x => new BankingEventData
+            {
+                AggregateId = x.AggregateId,
+                Data = JsonConvert.SerializeObject(x),
+                EventType = x.GetType().AssemblyQualifiedName!,
+                OccurredOn = x.OccurredOn,
+                Section = "banking"
+            });
             await _cosmosDbContext
                 .Set<BankingEventData>()
-                .AddRangeAsync(domainEvents.Select(x => new BankingEventData
-                {
-                    AggregateId = x.AggregateId,
-                    Data = JsonConvert.SerializeObject(x),
-                    EventType = x.GetType().FullName!,
-                    OccurredOn = x.OccurredOn,
-                }), cancellationToken);
+                .AddRangeAsync(entities, cancellationToken);
 
             await _cosmosDbContext.SaveChangesAsync(cancellationToken);
         }
