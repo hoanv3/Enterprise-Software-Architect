fe3c25ece7cda4353795f87a56dd4e25e83114b4
LQA\steven <Steven@ltsgroup.tech>
2025-05-02 09:35:27 +0700

    gRPC skeleton creation.

---
+++ ReportingSolution.gRPC.Server/Services/BlogPostService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using ReportingSolution.gRPC.Server.Protos;
+
+namespace ReportingSolution.gRPC.Server.Services
+{
+    public class BlogPostService : BlogPost.BlogPostBase
+    {
+        public override Task<CreateBlogPostResponse> CreateBlogPost(CreateBlogPostRequest request, ServerCallContext context)
+        {
+            return base.CreateBlogPost(request, context);
+        }
+    }
+}
---
+++ src/ReportingSolution.gRPC.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+
+namespace ReportingSolution.gRPC.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using var gRpcChannel = GrpcChannel.ForAddress("https://localhost:7234");
+        }
+    }
+}
